4cbd42d32fe9ad7d96d04c17e1baca4409f75d9f
Aeelen Miranda <77294891+Aeelen@users.noreply.github.com>
2021-03-02 13:53:08 -0600

    Update dash.py

--- application/dash.py
+++ application/dash.py
@@ -539,4 +539,4 @@ from application.dash import app
 from settings import config
 
 if __name__ == "__main__":
-    app.run_server(debug=config.debug, host=config.host, port=config.port)
+    app.run_server()
