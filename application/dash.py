e1f9f8cd00516f929b7ade33177f7599adae57f7
Felipe de Alba <45267327+fdealbam@users.noreply.github.com>
2021-03-12 11:45:05 -0600

    Update dash.py

--- application/dash.py
+++ application/dash.py
@@ -1,5 +1,7 @@
 import dash
 import dash_bootstrap_components as dbc
+import dash
+import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
@@ -652,9 +654,9 @@ body = html.Div([
 # Fecha de actualización
     
        dbc.Row(
-           [dbc.Col(html.H6(d2 ),
+           [dbc.Col(html.H6([str(d2), ",  Fuente: Secretaría de Salud"]),
                width={'size' : "auto",
-                      'offset' : 5,
+                      'offset' : 4,
             }), 
             ]),
     
